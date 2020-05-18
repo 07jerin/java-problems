f7af6c2d7c4ee9624faa08f6d69560031ded1e41
Jerin Joseph <jerin.joseph@oracle.com>
2020-05-18 23:45:15 +0530

    Initial commit

---
+++ src/com/test/samples/BooleanTry.java
@@ -0,0 +1,14 @@
+package com.test.samples;
+
+public class BooleanTry {
+	static Long l;
+	
+	public static void main(String[] args) {
+		Boolean b = (Boolean)null;
+		if(Boolean.TRUE.equals(b)) System.out.println("SS");
+		System.out.println();
+		
+		System.out.println(l);
+	}
+
+}
